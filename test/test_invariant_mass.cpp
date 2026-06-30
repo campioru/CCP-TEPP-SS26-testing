5421b9375ab0499ea156c25cc619943eb68125ff
Ruaidhrí Campion <campioru@tcd.ie>
2026-06-30 11:57:51 +0100

    initial GTest setup, ignoring CMake stuff

--- test/test_invariant_mass.cpp
+++ test/test_invariant_mass.cpp
@@ -1,32 +1,15 @@
 //! \file test_invariant_mass.cpp
-#include <cmath>
-#include <stdexcept>
-#include <iostream>
-#include <cassert>
 #include "invariant_mass.hpp"
+#include <gtest/gtest.h>
 
 // Case 1. Physical domain
-void test_physical_domain()
+TEST(InvariantMass, PhysicalDomain)
 {
-  assert((invariant_mass(100., 100.) == 0.) && "mass of photon not zero");
+  FAIL() << "not implemented yet" << std::endl;
 }
 
 // Case 2. Error conditions
-void test_error_conditions()
+TEST(InvariantMass, UnphysicalEnergies)
 {
-  try
-  {
-    double bad = invariant_mass(-1, 0);
-    assert(false && "std::domain_error not thrown for negative error");
-  }
-  catch(const std::domain_error&)
-  {
-    std::cout << "pass" << std::endl;
-  }
-}
-
-int main()
-{
-  test_physical_domain();
-  test_error_conditions();
+  FAIL() << "not implemented yet" << std::endl;
 }
