6ab79efc6c797ce6ff5ea4f3457e11d49b7fbcc4
Ruaidhrí Campion <campioru@tcd.ie>
2026-07-01 09:37:03 +0100

    eq -> double eq

--- test/test_invariant_mass.cpp
+++ test/test_invariant_mass.cpp
@@ -5,11 +5,18 @@
 // Case 1. Physical domain
 TEST(InvariantMass, PhysicalDomain)
 {
-  EXPECT_EQ(invariant_mass(10., 0.), 10.) << "at rest, particle does not have correct mass" << std::endl;
-  EXPECT_EQ(invariant_mass(100., 100.), 0.) << "massless particle is not massless" << std::endl;
+  EXPECT_DOUBLE_EQ(invariant_mass(10., 0.), 10.) << "at rest, particle does not have correct mass" << std::endl;
+  EXPECT_DOUBLE_EQ(invariant_mass(100., 100.), 0.) << "massless particle is not massless" << std::endl;
 
-  EXPECT_EQ(invariant_mass(5, 3), 4) << "off mass shell" << std::endl;
-  EXPECT_EQ(invariant_mass(5, 4), 3) << "off mass shell" << std::endl;
+  EXPECT_DOUBLE_EQ(invariant_mass(5, 3), 4) << "off mass shell" << std::endl;
+  EXPECT_DOUBLE_EQ(invariant_mass(5, 4), 3) << "off mass shell" << std::endl;
+
+  EXPECT_DOUBLE_EQ(invariant_mass(.5, .3), .4) << "off mass shell" << std::endl;
+  EXPECT_DOUBLE_EQ(invariant_mass(.5, .4), .3) << "off mass shell" << std::endl;
+
+  EXPECT_DOUBLE_EQ(.1 + .2, .3);
+  EXPECT_DOUBLE_EQ(.1 + .2, .2 + .1);
+  EXPECT_DOUBLE_EQ((.1 + .3) + .2, .1 + (.3 + .2));
 }
 
 // Case 2. Error conditions
