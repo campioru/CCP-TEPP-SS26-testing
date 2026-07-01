0330d445d4da3cc52a79133a3c8ce0c82ab83573
Ruaidhrí Campion <campioru@tcd.ie>
2026-07-01 10:57:27 +0100

    first few hist tests

--- test/test_histogram.cpp
+++ test/test_histogram.cpp
@@ -3,3 +3,18 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+
+TEST(HistogramConstruction, ValidParametersDoNotThrow)
+{
+  EXPECT_NO_THROW(Histogram(10, 0.0f, 1.0f));
+}
+
+TEST(HistogramConstruction, ZeroBinsThrows)
+{
+  EXPECT_THROW(Histogram(0, 0.0f, 10.0f), std::invalid_argument);
+}
+
+TEST(HistogramConstruction, NegativeBinsThrows)
+{
+  EXPECT_THROW(Histogram(-1, 0.0f, 10.0f), std::invalid_argument);
+}
