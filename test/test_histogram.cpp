9410a1f13bcbb347255b9d8b7c96615564718291
Ruaidhrí Campion <campioru@tcd.ie>
2026-07-01 11:23:16 +0100

    remaining hist tests, including expect_that

--- test/test_histogram.cpp
+++ test/test_histogram.cpp
@@ -18,3 +18,23 @@ TEST(HistogramConstruction, NegativeBinsThrows)
 {
   EXPECT_THROW(Histogram(-1, 0.0f, 10.0f), std::invalid_argument);
 }
+
+TEST(HistogramConstruction, IncorrectRangeThrows)
+{
+  EXPECT_THROW(Histogram(10, 10.0f, 0.0f), std::invalid_argument);
+}
+
+TEST(HistogramConstruction, BinCountsHasCorrectSize)
+{
+  EXPECT_EQ(Histogram(10, 0.0f, 10.0f).bin_counts().size(), 10);
+  EXPECT_EQ(Histogram(1, 0.0f, 10.0f).bin_counts().size(), 1);
+}
+
+TEST(HistogramConstruction, AllBinsInitiallyZero)
+{
+  Histogram hist(10, 0.0f, 1.0f);
+  std::vector<float> expected(10, 0.0f);
+
+  // EXPECT_EQ(hist.bin_counts(), expected);
+  EXPECT_THAT(hist.bin_counts(), ::testing::ContainerEq(expected));
+}
